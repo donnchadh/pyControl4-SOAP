4c1f13381f9ac8a35067f132409a8ad50b121c12
lawtancool <26829131+lawtancool@users.noreply.github.com>
2020-06-07 19:30:54 -0700

    Python3 string encoding fix

--- Modules/PyControl4.py
+++ Modules/PyControl4.py
@@ -32,7 +32,7 @@ class C4SoapConn:
     
     @staticmethod
     def Send(MESSAGE):
-         socketConn.sendall(MESSAGE + "\0")
+         socketConn.sendall((MESSAGE + "\0").encode())
 
 class C4Light:
     '''
@@ -50,7 +50,7 @@ class C4Light:
     '''
     def setLevel(self,value):
         MESSAGE = '<c4soap name="SendToDeviceAsync" async="1"><param name="data" type="STRING"><devicecommand><command>SET_LEVEL</command><params><param><name>LEVEL</name><value type="INT"><static>%d</static></value></param></params></devicecommand></param><param name="idDevice" type="INT">%d</param></c4soap>' % (value, self.id)
-        socketConn.sendall(MESSAGE + "\0")
+        socketConn.sendall((MESSAGE + "\0").encode())
     
     '''
     Ramps to a specified level in milliseconds
@@ -60,7 +60,7 @@ class C4Light:
     '''
     def rampToLevel(self,percent, time):
         MESSAGE = '<c4soap name="SendToDeviceAsync" async="1"><param name="data" type="STRING"><devicecommand><command>RAMP_TO_LEVEL</command><params><param><name>TIME</name><value type="INTEGER"><static>%d</static></value></param><param><name>LEVEL</name><value type="PERCENT"><static>%d</static></value></param></params></devicecommand></param><param name="idDevice" type="INT">%d</param></c4soap>' % (time, percent, self.id)
-        socketConn.sendall(MESSAGE + "\0")
+        socketConn.sendall((MESSAGE + "\0").encode())
     
     '''
     Returns the light level for a dimmer. Value between 0 and 100.
@@ -68,7 +68,7 @@ class C4Light:
     '''
     def getLevel(self):
         MESSAGE = '<c4soap name="GetVariable" async="False"><param name = "iddevice" type = "INT">%d</param><param name = "idvariable" type = "INT">1001</param></c4soap>' % (self.id)
-        socketConn.sendall(MESSAGE + "\0")
+        socketConn.sendall((MESSAGE + "\0").encode())
         data = socketConn.recv(BUFFER_SIZE)
         data = BeautifulSoup(data)
         value = data.find("variable")
@@ -81,7 +81,7 @@ class C4Light:
     '''
     def getLightState(self):
         MESSAGE = '<c4soap name="GetVariable" async="False"><param name = "iddevice" type = "INT">%d</param><param name = "idvariable" type = "INT">1000</param></c4soap>' % (self.id)
-        socketConn.sendall(MESSAGE + "\0")
+        socketConn.sendall((MESSAGE + "\0").encode())
         data = socketConn.recv(BUFFER_SIZE)
         data = BeautifulSoup(data)
         value = data.find("variable")
@@ -92,17 +92,17 @@ class C4Light:
 class C4Remote:
     def VolDown(self):
         MESSAGE = '<c4soap name="SendToDeviceAsync" async="1"><param name="iddevice" type="number">10</param><param name="data" type="string"><devicecommand><command>PULSE_VOL_DOWN</command><params></params></devicecommand></param></c4soap>'
-        socketConn.sendall(MESSAGE + "\0")
+        socketConn.sendall((MESSAGE + "\0").encode())
         
     def VolUp(self):
         MESSAGE = '<c4soap name="SendToDeviceAsync" async="1"><param name="iddevice" type="number">10</param><param name="data" type="string"><devicecommand><command>PULSE_VOL_UP</command><params></params></devicecommand></param></c4soap>'
-        socketConn.sendall(MESSAGE + "\0")
+        socketConn.sendall((MESSAGE + "\0").encode())
         
     def Info(self):
         MESSAGE = '<c4soap name="SendToDeviceAsync" async="1"><param name="iddevice" type="number">10</param><param name="data" type="string"><devicecommand><command>INFO</command><params></params></devicecommand></param></c4soap>'
-        socketConn.sendall(MESSAGE + "\0")
+        socketConn.sendall((MESSAGE + "\0").encode())
         
     def Cancel(self):
         MESSAGE = '<c4soap name="SendToDeviceAsync" async="1"><param name="iddevice" type="number">10</param><param name="data" type="string"><devicecommand><command>CANCEL</command><params></params></devicecommand></param></c4soap>'
-        socketConn.sendall(MESSAGE + "\0")
+        socketConn.sendall((MESSAGE + "\0").encode())
         
