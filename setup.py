b74474083905247e73b91b24d682d0d2a223bc5e
lawtancool <26829131+lawtancool@users.noreply.github.com>
2020-06-08 11:37:31 -0700

    update setup.py details

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="PyControl4", # Replace with your own username
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="lawtancool",
+    author_email="contact@lawrencetan.ml",
+    description="Python library for interfacing with Control4 systems via C4soap",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/lawtancool/pyControl4",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
